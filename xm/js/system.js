8e27a7f0391a6f64bf04c90c8deb966ef9eb2072
sato2333 <82182565+sato2333@users.noreply.github.com>
2021-05-17 16:25:19 +0800

    Update system.js

--- xm/js/system.js
+++ xm/js/system.js
@@ -66,15 +66,9 @@ var feifei = {
 	},
 	'pushBaidu':function(){
 		var bp = document.createElement('script');
-    var curProtocol = window.location.protocol.split(':')[0];
-    if (curProtocol === 'https') {
-        bp.src = 'https://zz.bdstatic.com/linksubmit/push.js';
-    }
-    else {
-        bp.src = 'http://push.zhanzhang.baidu.com/push.js';
-    }
-    var s = document.getElementsByTagName("script")[0];
-    s.parentNode.insertBefore(bp, s);
+		bp.src = '//zz.bdstatic.com/linksubmit/push.js';
+		var s = document.getElementsByTagName("script")[0];
+		s.parentNode.insertBefore(bp, s);
 	}
 },
 'mobile':{//移动端专用
