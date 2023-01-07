81933e38d4c0b315e2938a15f18a19281cb301e7
sato2333 <82182565+sato2333@users.noreply.github.com>
2023-01-07 20:39:51 +0800

    Update cb.js

--- dz/s/js/cb.js
+++ dz/s/js/cb.js
@@ -1,7 +1,7 @@
 var rd=[
-	'ΦΦpQwAto7B5u8ξ',
-	'ζdEZikM9B5u8ё',
-	'ζ6ijbLpaB5u8￠￠',
+	'9 a:/ 声解叔名些听究但杍',
+	'9 a:/ 由见屋眼菜于究但燥',
+	'4 y:/ 并名更就结于究但毓',
 ];
 var clipboard = new Clipboard('a',{
 	text:()=>{
