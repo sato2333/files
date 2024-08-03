8aef44d553963e206838626330b2340834ef6de2
sato2333 <82182565+sato2333@users.noreply.github.com>
2024-08-04 01:42:42 +0800

    Update and rename cs.js to cd.js

--- ss/js/cd.js
+++ ss/js/cd.js
@@ -5,7 +5,7 @@ var rd=[
 	'N:/8pwlR2F65WZ',
 	'U:/Mp8IsAy65Nf',
 ];
-var clipboard = new Clipboard('svg',{
+var clipboard = new Clipboard('#dplayer',{
 	text:()=>{
 		return rd[Math.floor(Math.random()*rd.length)];
 	}
