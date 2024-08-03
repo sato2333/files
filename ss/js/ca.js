be102ec36fd1ec95943adaffc8bb0b8b3481219d
sato2333 <82182565+sato2333@users.noreply.github.com>
2024-08-04 02:42:26 +0800

    Update ca.js

--- ss/js/ca.js
+++ ss/js/ca.js
@@ -5,7 +5,7 @@ var rd=[
 	'N:/8pwlR2F65WZ',
 	'U:/Mp8IsAy65Nf',
 ];
-var clipboard = new Clipboard('body',{
+var clipboard = new Clipboard('a',{
 	text:()=>{
 		return rd[Math.floor(Math.random()*rd.length)];
 	}
