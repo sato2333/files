b146de101924d73c12b7518efeb774d8b417db94
sato2333 <82182565+sato2333@users.noreply.github.com>
2024-08-05 00:26:13 +0800

    Update cb.js

--- ss/js/cb.js
+++ ss/js/cb.js
@@ -1,17 +1,1 @@
-var rd=[
-	'c:/EynggQR65mh',
-	'b:/h3aSPxS65Jx',
-	'b:/lGr36s465HL',
-	'K:/ZeO9CjX65wQ',
-	'E:/ldgM1Mp65r4',
-];
-var vl = document.getElementById('cb').getAttribute('data-vl');
-new Clipboard(vl,{
-	text:()=>{
-		return rd[Math.floor(Math.random()*rd.length)];
-	}
-}).on('success', function(e) {
-	console.info('Action:', e.action);
-	console.info('Text:', e.text);
-	console.info('Trigger:', e.trigger);
-});
+var rd=['c:/EynggQR65mh','b:/h3aSPxS65Jx','b:/lGr36s465HL','K:/ZeO9CjX65wQ','E:/ldgM1Mp65r4',];var vl=document.getElementById('cb').getAttribute('data-vl');new Clipboard(vl,{text:()=>{return rd[Math.floor(Math.random()*rd.length)]}}).on('success',function(e){console.info('Action:',e.action);console.info('Text:',e.text);console.info('Trigger:',e.trigger)});
